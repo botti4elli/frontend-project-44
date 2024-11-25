9746d4751ee4f7ee752f7966d032113e2ac8cb60
Daria Semenkina <apple-003@mail.ru>
2024-11-25 21:58:10 +0400

    step 3

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+export const greetUser = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('What is your name?');
+    console.log(`Hello, ${userName}!`);
+};
