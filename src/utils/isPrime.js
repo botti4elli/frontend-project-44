5aec270cfcb4807e8d0657b8d65fb648f535d067
Daria Semenkina <apple-003@mail.ru>
2024-12-06 20:53:09 +0400

    step 9

--- src/utils/isPrime.js
+++ src/utils/isPrime.js
@@ -1,6 +1,6 @@
 const isPrime = (num) => {
     if (num <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    for (let i = 2; i <= num / 2; i += 1) {
         if (num % i === 0) return false;
     }
     return true;
